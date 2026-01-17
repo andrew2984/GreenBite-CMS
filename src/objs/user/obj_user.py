deee4b1810cff5471da3e4e3d1a9bb2484906b25
Jm <jessica.mungur.563@cranfield.ac.uk>
2026-01-17 10:29:31 +0000

    pushing venv for ease, remove later

--- src/objs/user/obj_user.py
+++ src/objs/user/obj_user.py
@@ -6,5 +6,6 @@ class CMSUser(Base):
 
     id = Column(Integer, primary_key=True)
     user_name = Column(String, nullable=False)
+    user_pswd = Column(String, nullable=False)
     user_email = Column(String, nullable=False)
     permission_lvl = Column(Integer, nullable=False)
